721643adc7159458d5e9737ddabd30e60c99854c
Kenneth Korcal <kakorcal@gmail.com>
2016-10-03 01:17:31 -0700

    redirect after signup

--- client/components/partials/SignupForm.js
+++ client/components/partials/SignupForm.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import validateInput from '../../../server/shared/validations/signup'
 import TextFieldGroup from './common/TextFieldGroup'
+import {browserHistory} from 'react-router'
 
 export default class SignupForm extends Component{
   constructor(props) {
@@ -34,6 +35,7 @@ export default class SignupForm extends Component{
       this.props.userSignupRequest(this.state)
         .then(res=>{
           console.log(res.data);
+          browserHistory.push('/');
         }).catch(err=>{
           console.log(err.response.data);
           this.setState({errors: err.response.data, isLoading: false});
